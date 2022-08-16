76fe59b5d9a5c132e54f625c7ab8aa13e20a2775
Randa Bukowski <randabukowski@Randas-MacBook-Pro.local>
2022-08-15 21:53:12 -0700

    created my variables

---
+++ script.js
@@ -0,0 +1,18 @@
+//Made the variables I will use throughout the code.
+
+var section1 = document.getElementById("section1");
+var section2 = document.getElementById("section2");
+var section3 = document.getElementById("section3");
+var section4 = document.getElementById("section4");
+var question = document.getElementById("question");
+var start = document.getElementById("startbtn");
+var choice1 = document.getElementById("choice1");
+var choice2 = document.getElementById("choice2");
+var choice3 = document.getElementById("choice3");
+var choice4 = document.getElementById("choice4");
+var counter = 0
+var input = document.getElementById("initials");
+var save = document.getElementById("save");
+var timeEl = document.getElementById("time");
+var timeInterval 
+var playAgain = document.getElementById("playAgain");
